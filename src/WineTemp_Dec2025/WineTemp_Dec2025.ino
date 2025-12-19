e3452a1ab7304b2c399f399641db9ac15d787920
Mike Dodds <mdodds@lumen.bio>
2025-12-19 09:05:06 -0800

    WineTemp sensor integrated

--- src/WineTemp_Dec2025/WineTemp_Dec2025.ino
+++ src/WineTemp_Dec2025/WineTemp_Dec2025.ino
@@ -1,23 +1,6 @@
-// Compile for Adafruit Feather HUZZAH ESP8266.
+// Compile for LOLIN(WEMOS) D1 mini pro (ESP8266)
 // 80 MHz
 
-/***************************************************
-  Adafruit MQTT Library ESP8266 Example
-
-  Must use ESP8266 Arduino from:
-    https://github.com/esp8266/Arduino
-
-  Works great with Adafruit's Huzzah ESP board & Feather
-  ----> https://www.adafruit.com/product/2471
-  ----> https://www.adafruit.com/products/2821
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Tony DiCola for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
 #include <ESP8266WiFi.h>
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
@@ -26,12 +9,8 @@
 #include "Metro.h"
 #include "Streaming.h"
 
-/************************* WiFi Access Point *********************************/
-
 #define WLAN_SSID "Looney"
 #define WLAN_PASS "TinyandTooney"
-
-/************************* Adafruit.io Setup *********************************/
 
 #define SERVER "broker.hivemq.com"
 #define SERVERPORT 1883  // use 8883 for SSL
@@ -43,105 +22,87 @@ DHTesp dht;
 
 // Create an ESP8266 WiFiClient class to connect to the MQTT server.
 WiFiClient client;
-// or... use WiFiClientSecure for SSL
-//WiFiClientSecure client;
 
 // Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
 Adafruit_MQTT_Client mqtt(&client, SERVER, SERVERPORT, CLIENTNAME);
 
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'photocell' for publishing.
-Adafruit_MQTT_Publish pubData = Adafruit_MQTT_Publish(&mqtt, "magister/weather/sensor/indoor/wine");
-
-/*************************** Sketch Code ************************************/
-
 void setup() {
   Serial.begin(115200);
-  delay(10);
+  Serial.flush();
+  delay(100UL);
+
+  Serial << endl << endl << "Startup." << endl;
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);  // light on.
 
   String thisBoard = ARDUINO_BOARD;
   Serial.println(thisBoard);
 
-  dht.setup(D4, DHTesp::DHT11);  // Connect DHT sensor to D4
+  // Connect DHT sensor to D4
+  dht.setup(D4, DHTesp::DHT11);
+  // get data
+  TempAndHumidity values = dht.getTempAndHumidity();
+
+  // Manually assign struct members to JSON keys
+  StaticJsonDocument<200> doc;
+  doc["Temperature"] = values.temperature;
+  doc["Humidity"] = values.humidity;
+  doc["Interval"] = SEND_INTERVAL / 1000UL;
+
+  char output[200];
+  serializeJson(doc, output, sizeof(output));
+
+  // Now we can publish stuff!
+  Serial << "Sending: " << output << endl;
 
   // Connect to WiFi access point.
-  Serial.println();
-  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(WLAN_SSID);
 
   WiFi.begin(WLAN_SSID, WLAN_PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    TempAndHumidity values = dht.getTempAndHumidity();
-  }
-  Serial.println();
+  while (WiFi.status() != WL_CONNECTED) delay(1);
 
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  Serial << "Setup.  complete." << endl;
-
-  pinMode(BUILTIN_LED, OUTPUT);
-  digitalWrite(BUILTIN_LED, HIGH);  // light on.
-}
-
-void loop() {
-
-  // Ensure the connection to the MQTT server is alive (this will make the first
-  MQTT_connect();
-
-  if (mqtt.connected()) {
-    // get data
-    TempAndHumidity values = dht.getTempAndHumidity();
-
-    // Manually assign struct members to JSON keys
-    StaticJsonDocument<200> doc;
-    doc["Temperature"] = values.temperature;
-    doc["Humidity"] = values.humidity;
-    doc["Interval"] = SEND_INTERVAL/1000UL;
-
-    char output[200];
-    serializeJson(doc, output, sizeof(output));
-
-    // Now we can publish stuff!
-    Serial << "Sending: " << output << " ... ";
-    if (!pubData.publish(output)) {
-      Serial << "Failed!" << endl;
-    } else {
-      Serial << "Success." << endl;
-    }
-  } 
-
-  delay(SEND_INTERVAL);
-}
-
-// Function to connect and reconnect as necessary to the MQTT server.
-// Should be called in the loop function and it will take care if connecting.
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    digitalWrite(BUILTIN_LED, LOW);
-
-    return;
-  }
-  digitalWrite(BUILTIN_LED, HIGH);
-
   Serial.print("Connecting to MQTT... ");
-
   static uint8_t retries = 100;
+  int8_t ret;
   while ((ret = mqtt.connect()) != 0) {  // connect will return 0 for connected
     Serial.println(mqtt.connectErrorString(ret));
     Serial.println("Retrying MQTT connection in 5 seconds...");
     mqtt.disconnect();
+    delay(5000UL);
     retries--;
     if (retries == 0) {
       return;
     }
   }
   Serial.println("MQTT Connected!");
+
+  // Setup a feed  for publishing.
+  String pub = "magister/weather/sensor/" + String(WiFi.macAddress());
+  Adafruit_MQTT_Publish pubData = Adafruit_MQTT_Publish(&mqtt, pub.c_str());
+
+  Serial << "Publishing to " << pub << endl;
+  if (!pubData.publish(output)) {
+    Serial << "Publish Failed!" << endl;
+  } else {
+    Serial << "Publish Success." << endl;
+  }
+
+  digitalWrite(LED_BUILTIN, HIGH);  // light off.
+  delay(5000UL);
+  Serial << "Sleeping...." << endl;
+
+  ESP.deepSleep(SEND_INTERVAL * 1000UL); // microseconds
+
+}
+
+void loop() {
+  // never reached
+  Serial << "Didn't sleep!" << endl;
+  delay(500);
 }
